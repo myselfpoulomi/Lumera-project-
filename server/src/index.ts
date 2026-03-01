57f32ef54987e5dafee411f35955dfd16b56498e
myselfpoulomi <routhpoulomi@gmail.com>
2026-03-01 18:38:23 +0530

    set up

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+
+
+const app=express();
+
+const PORT=3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Node + TypeScript is working 🚀");
+}); 
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
