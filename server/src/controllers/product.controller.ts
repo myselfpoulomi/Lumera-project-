14eb47db83766aca54aa91dcb0a9afbbd1f1e350
myselfpoulomi <routhpoulomi@gmail.com>
2026-03-04 12:56:33 +0530

    created get product by id route

--- server/src/controllers/product.controller.ts
+++ server/src/controllers/product.controller.ts
@@ -55,4 +55,21 @@ async function getProducts(req: Request, res: Response) {
     }   
 }
 
-export { createProduct, getProducts };
+
+async function getProductByID(req: Request<{ id: string }>, res: Response) {
+    const { id } = req.params;   
+    try {
+        const product = await prisma.product.findUnique({
+            where: { id },
+        }); 
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.status(200).json(product);
+    } catch (error: any) {
+        console.error("Error fetching product:", error);
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }    
+}
+
+export { createProduct, getProducts, getProductByID};
--- server/src/routers/product.routes.ts
+++ server/src/routers/product.routes.ts
@@ -1,11 +1,11 @@
-import { get } from "http";
-import { createProduct , getProducts } from "../controllers/product.controller";
+import { createProduct , getProducts, getProductByID } from "../controllers/product.controller";
 import { Router } from "express";
 
 const router = Router();
 
 router.post("/create-product", createProduct);
 router.get("/get-products", getProducts)
+router.get("/get-product/:id", getProductByID)
 
 
 export default router;
